062dbc6ac99938acf87307aa5d7b4cf435c6206c
Érico Andrei <ericof@gmail.com>
2025-04-22 13:51:58 -0300

    Refactor block registration

---
+++ packages/volto-social-blocks/src/config/blocks.ts
@@ -0,0 +1,186 @@
+import type { ConfigType as BaseConfigType } from '@plone/registry';
+import type { BlocksConfig, BlockConfigBase } from '@plone/types';
+
+import cloneDeep from 'lodash/cloneDeep';
+
+// Facebook
+import facebookSVG from '../icons/facebook.svg';
+import { facebookSchema } from '../components/Blocks/Facebook/schema';
+import FacebookBlockView from '../components/Blocks/Facebook/View';
+import FacebookBlockEdit from '../components/Blocks/Facebook/Edit';
+
+// Instagram
+import instagramSVG from '../icons/instagram.svg';
+import { instagramSchema } from '../components/Blocks/Instagram/schema';
+import InstagramBlockView from '../components/Blocks/Instagram/View';
+import InstagramBlockEdit from '../components/Blocks/Instagram/Edit';
+
+// Twitter
+import twitterSVG from '../icons/twitter.svg';
+import { tweetSchema } from '../components/Blocks/Tweet/schema';
+import TweetBlockView from '../components/Blocks/Tweet/View';
+import TweetBlockEdit from '../components/Blocks/Tweet/Edit';
+
+// Flickr
+import flickrSVG from '../icons/flickr.svg';
+import { flickrSchema } from '../components/Blocks/Flickr/schema';
+import FlickrBlockView from '../components/Blocks/Flickr/View';
+import FlickrBlockEdit from '../components/Blocks/Flickr/Edit';
+
+// Spotify
+import spotifySVG from '../icons/spotify.svg';
+import { spotifySchema } from '../components/Blocks/Spotify/schema';
+import SpotifyBlockView from '../components/Blocks/Spotify/View';
+import SpotifyBlockEdit from '../components/Blocks/Spotify/Edit';
+
+// Soundcloud
+import soundcloudSVG from '../icons/soundcloud.svg';
+import { soundcloudSchema } from '../components/Blocks/Soundcloud/schema';
+import SoundcloudBlockView from '../components/Blocks/Soundcloud/View';
+import SoundcloudBlockEdit from '../components/Blocks/Soundcloud/Edit';
+
+declare module '@plone/types' {
+  export interface BlocksConfigData {
+    facebookBlock: BlockConfigBase;
+    flickrBlock: BlockConfigBase;
+    instagramBlock: BlockConfigBase;
+    soundcloudBlock: BlockConfigBase;
+    spotifyBlock: BlockConfigBase;
+    tweetBlock: BlockConfigBase;
+  }
+}
+
+export interface CustomBlocksConfig
+  extends Omit<BlocksConfig, 'groupBlocksOrder'> {
+  groupBlocksOrder: { id: string; title: string }[];
+}
+export interface ConfigType extends Omit<BaseConfigType, 'blocks'> {
+  blocks: CustomBlocksConfig;
+}
+
+function installGroupBlocksOrder(config: ConfigType) {
+  // Blocks Groups
+  config.blocks.groupBlocksOrder = [
+    ...config.blocks.groupBlocksOrder,
+    { id: 'social', title: 'Social' },
+  ];
+  return config;
+}
+
+function addBlocksToOtherBlocks(config: ConfigType) {
+  // Array of local blocks ids
+  const localBlocks = [
+    'facebookBlock',
+    'flickrBlock',
+    'instagramBlock',
+    'soundcloudBlock',
+    'spotifyBlock',
+    'tweetBlock',
+  ];
+
+  // Add Blocks to gridBlock and accordionBlock
+  // It's important to maintain the chain, and do not introduce pass by reference in
+  // the internal `blocksConfig` object, so we clone the object to avoid this.
+  ['__grid', 'gridBlock', 'accordion'].forEach((blockId) => {
+    const block = config.blocks.blocksConfig[blockId];
+    if (
+      block !== undefined &&
+      block.allowedBlocks !== undefined &&
+      block.blocksConfig !== undefined
+    ) {
+      block.allowedBlocks = [...block.allowedBlocks, ...localBlocks];
+      localBlocks.forEach((blockId) => {
+        block.blocksConfig[blockId] = cloneDeep(
+          config.blocks.blocksConfig[blockId],
+        );
+      });
+    }
+  });
+}
+
+export default function install(config: ConfigType) {
+  installGroupBlocksOrder(config);
+  // Blocks
+  config.blocks.blocksConfig.facebookBlock = {
+    id: 'facebookBlock',
+    title: 'Facebook',
+    icon: facebookSVG,
+    group: 'social',
+    category: 'embed',
+    blockSchema: facebookSchema,
+    view: FacebookBlockView,
+    edit: FacebookBlockEdit,
+    restricted: false,
+    mostUsed: false,
+    sidebarTab: 1,
+  };
+  config.blocks.blocksConfig.flickrBlock = {
+    id: 'flickrBlock',
+    title: 'Flickr',
+    icon: flickrSVG,
+    group: 'social',
+    category: 'embed',
+    blockSchema: flickrSchema,
+    view: FlickrBlockView,
+    edit: FlickrBlockEdit,
+    restricted: false,
+    mostUsed: false,
+    sidebarTab: 1,
+  };
+  config.blocks.blocksConfig.instagramBlock = {
+    id: 'instagramBlock',
+    title: 'Instagram',
+    icon: instagramSVG,
+    group: 'social',
+    category: 'embed',
+    blockSchema: instagramSchema,
+    view: InstagramBlockView,
+    edit: InstagramBlockEdit,
+    restricted: false,
+    mostUsed: false,
+    sidebarTab: 1,
+  };
+  config.blocks.blocksConfig.spotifyBlock = {
+    id: 'spotifyBlock',
+    title: 'Spotify',
+    icon: spotifySVG,
+    group: 'social',
+    category: 'embed',
+    blockSchema: spotifySchema,
+    view: SpotifyBlockView,
+    edit: SpotifyBlockEdit,
+    restricted: false,
+    mostUsed: false,
+    sidebarTab: 1,
+  };
+  config.blocks.blocksConfig.soundcloudBlock = {
+    id: 'soundcloudBlock',
+    title: 'Soundcloud',
+    icon: soundcloudSVG,
+    group: 'social',
+    category: 'embed',
+    blockSchema: soundcloudSchema,
+    view: SoundcloudBlockView,
+    edit: SoundcloudBlockEdit,
+    restricted: false,
+    mostUsed: false,
+    sidebarTab: 1,
+  };
+  config.blocks.blocksConfig.tweetBlock = {
+    id: 'tweetBlock',
+    title: 'Tweet',
+    icon: twitterSVG,
+    group: 'social',
+    category: 'embed',
+    blockSchema: tweetSchema,
+    view: TweetBlockView,
+    edit: TweetBlockEdit,
+    restricted: false,
+    mostUsed: true,
+    sidebarTab: 1,
+    defaultLanguage: 'en',
+    defaultTheme: 'light',
+  };
+  addBlocksToOtherBlocks(config);
+  return config;
+}
